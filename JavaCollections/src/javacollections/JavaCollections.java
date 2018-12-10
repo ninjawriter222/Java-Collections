f6d191ed0cad627165e3ca291434b1a88ce19b84
ninjawriter222 <devin.a.poe@gmail.com>
2018-12-10 15:23:44 -0700

    Initial commit

---
+++ JavaCollections/src/javacollections/JavaCollections.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javacollections;
+import java.io.*;
+import java.util.*;
+/**
+ *
+ * @author Poozer
+ */
+public class JavaCollections 
+{
+    public static void main(String[] args) {
+       int arr[] = new int[] {1, 2, 3, 4};
+       Vector<Integer> x = new Vector(); 
+       Hashtable<Integer, String> z = new Hashtable(); 
+       x.addElement(1); 
+       x.addElement(2); 
+       z.put(1,"Bort"); 
+       z.put(2,"4Bort");
+       
+        System.out.println(arr[0]); 
+        System.out.println(x.elementAt(0)); 
+        System.out.println(z.get(1)); 
+    }
+}
